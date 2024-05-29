c226a847f4fb7101b4ee853e3ef341652c76bfe0
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-05-29 21:00:31 +0300

    Use try catch and async

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
-
-  Promise.all(promises)
-    .then((response) => {
-      const [photoResponse, userResponse] = response;
-      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
+
 export default handleProfileSignup;
