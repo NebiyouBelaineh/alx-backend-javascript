10322c01c056c6531b40e7c26df86c05855175d9
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-05-29 20:27:24 +0300

    Add error message

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,12 @@ import { uploadPhoto, createUser } from './utils';
 
 async function handleProfileSignup() {
   const results = await Promise.all([uploadPhoto(), createUser()]);
-  console.log(
-    `${results[0].body} ${results[1].firstName} ${results[1].lastName}`
-  );
+  if (results.length < 2) {
+    console.log('Signup system offline');
+  } else {
+    console.log(
+      `${results[0].body} ${results[1].firstName} ${results[1].lastName}`
+    );
+  }
 }
 export default handleProfileSignup;
