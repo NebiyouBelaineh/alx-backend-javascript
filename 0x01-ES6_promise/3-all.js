aa7b637fa0e12cf8ba7ceb00bd7d4db5b7e3484a
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-05-29 18:14:27 +0300

    Fix eslint error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 async function handleProfileSignup() {
   const results = await Promise.all([uploadPhoto(), createUser()]);
   console.log(
-    `${results[0].body} ${results[1].firstName} ${results[1].lastName}`,
+    `${results[0].body} ${results[1].firstName} ${results[1].lastName}`
   );
 }
 export default handleProfileSignup;
