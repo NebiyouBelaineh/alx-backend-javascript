39cd9e86f51d1d305d9b2d2a2e7224c3aab84529
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-04 03:02:57 +0300

    Add solution for task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+function updateStudentGradeByCity(listStudents, city, newGrades) {
+  const locationFiltered = listStudents
+    .filter((student) => newGrades
+      .some((newGrade) => newGrade.studentId === student.id));
+
+  return locationFiltered.map((student) => {
+    const newGradeObj = newGrades.find((newGrade) => newGrade.studentId === student.id);
+    console.log(newGradeObj);
+    if (newGradeObj.grade) {
+      return { ...student, grade: newGradeObj.grade };
+    }
+    return { ...student, grade: 'N/A' };
+  });
+}
+export default updateStudentGradeByCity;
