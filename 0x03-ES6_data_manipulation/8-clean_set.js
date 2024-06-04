edccc328f53a74ad9ed06677846df3afe58fd7b0
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-04 21:13:57 +0300

    Add solution for task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,4 @@
 function cleanSet(set, startString) {
-  /* first we look if each set value starts with the startString
-  if these values exists, we truncate/remove the start string from
-  each set value matching our condition and save to an array
-  then we join the values using '-' */
   const len = startString.length;
   const strArr = [];
 
