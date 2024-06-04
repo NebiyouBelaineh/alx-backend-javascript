3b5e194e74dbb68a6f3e81a746a1939c4a19bbfb
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-04 21:21:12 +0300

    Return empty string for incompatible arguments

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 function cleanSet(set, startString) {
+  if (!(set instanceof Set) || (typeof startString !== 'string') || startString.length === 0) {
+    return '';
+  }
   const len = startString.length;
   const strArr = [];
 
   for (const value of set) {
-    if (value.startsWith(startString) && startString !== '') {
+    if (value.startsWith(startString)) {
       // console.log(value);
       strArr.push(value.slice(len));
     }
