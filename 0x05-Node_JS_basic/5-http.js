bcefc040382030b34a3bbca8658aa295375f8a98
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-23 20:09:56 +0300

    Add 5-http.js a server similar to 4-https.js that invokes countStudents based on /students route

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+/*
+create an HTTP server using the http module and handles routes '/'
+and '/students'
+*/
+
+const http = require('http');
+const stream = require('stream');
+
+const port = 12345;
+const countStudents = require('./3-read_file_async');
+
+function createConsoleCaptureStream() {
+  const captureStream = new stream.Writable();
+  let capturedOutput = '';
+
+  // eslint-disable-next-line
+  captureStream._write = (chunk, encoding, callback) => {
+    capturedOutput += chunk.toString();
+    capturedOutput += '\n';
+    callback();
+  };
+
+  return {
+    captureStream,
+    getCapturedOutput: () => {
+      if (capturedOutput.endsWith('\n')) {
+        capturedOutput = capturedOutput.slice(0, -1);
+      }
+      return capturedOutput;
+    },
+  };
+}
+// eslint-disable-next-line
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  const { captureStream, getCapturedOutput } = createConsoleCaptureStream();
+  const originalConsoleLog = console.log;
+  console.log = (...args) => {
+    // originalConsoleLog(...args);
+    captureStream.write(args.join(''));
+  };
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+      .then(() => {
+        // Replace original console.log
+        console.log = originalConsoleLog;
+        res.write('This is the list of our students\n');
+        // console.log(getCapturedOutput());
+        res.end(getCapturedOutput());
+      })
+      .catch((error) => {
+        console.log = originalConsoleLog;
+        res.end(error.message);
+      });
+  }
+});
+
+app.listen(port, () => {
+  // console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
