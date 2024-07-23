5e640907c5e059e109c4fbb65c430e78ac878fab
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-23 22:44:55 +0300

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,10 +4,36 @@ An express app
 */
 const express = require('express');
 const app = express();
+const stream = require('stream');
+
+const database = process.argv[2];
+console.log(typeof database, database);
 
 const countStudents = require('./3-read_file_async');
 
-const port = 12345;
+const port = 1245;
+
+function createConsoleCaptureStream() {
+  const captureStream = new stream.Writable();
+  let capturedOutput = '';
+
+  // eslint-disable-next-line
+  captureStream._write = (chunk, encoding, callback) => {
+    capturedOutput += chunk.toString();
+    capturedOutput += '\n';
+    callback();
+  };
+
+  return {
+    captureStream,
+    getCapturedOutput: () => {
+      if (capturedOutput.endsWith('\n')) {
+        capturedOutput = capturedOutput.slice(0, -1);
+      }
+      return capturedOutput;
+    },
+  };
+}
 
 // eslint-disable-next-line
 app.get('/', (req, res) => {
@@ -15,15 +41,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
-  countStudents('database.csv')
+  const { captureStream, getCapturedOutput } = createConsoleCaptureStream();
+  const originalConsoleLog = console.log;
+  console.log = (...args) => {
+    // originalConsoleLog(...args);
+    captureStream.write(args.join(''));
+  };
+  countStudents(database)
     .then(() => {
-      res.send('This is the list of our students\n');
+      // Replace original console.log
+      console.log = originalConsoleLog;
+      res.write('This is the list of our students\n');
+      // console.log(getCapturedOutput());
+      res.end(getCapturedOutput());
     })
     .catch((error) => {
-      res.send(error.message);
+      console.log = originalConsoleLog;
+      res.end(error.message);
     });
 });
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
