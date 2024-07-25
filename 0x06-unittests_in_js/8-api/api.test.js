514c7c1f84f9c0ac547948ee8b1a22dbfd8cf067
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-26 02:59:21 +0300

    Use request module instead of http and test for invlaid route

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,32 +5,31 @@
  */
 const expect = require('chai').expect;
 const exp = require('constants');
-// const app = require('./api');
-const http = require('http');
+const request = require('request');
 
 
 describe('api GET /', function () {
     const port = 7865;
     it('should return 200 statuscode, message and content-type', function (done) {
-        const request = http.request({
-            hostname: 'localhost',
-            port: port,
-            path: '/',
+        request({
+            url: `http://localhost:${port}/`,
             method: 'GET',
             headers: { 'Content-Type': 'text/plain' }
-        }, function (response) {
-            let data = '';
-
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-            response.on('end', function () {
-                expect(response.statusCode).to.equal(200);
-                expect(data).to.equal('Welcome to the payment system');
-                expect(response.headers['content-type']).to.equal('text/html; charset=utf-8');
-                done();
-            });
+        }, function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome to the payment system');
+            expect(response.headers['content-type']).to.equal('text/html; charset=utf-8');
+            done();
         });
-        request.end();
+    });
+    it('should return 404 statuscode for invalid route', function (done) {
+        request({
+            url: `http://localhost:${port}/invalid`,
+            method: 'GET',
+            headers: { 'Content-Type': 'text/plain' }
+        }, function (error, response, body) {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
     });
 });
