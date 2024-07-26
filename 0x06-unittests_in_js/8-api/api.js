ba2b27ba17d3b49e813f14384541a7bf507b4046
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-26 03:10:40 +0300

    Remove setting statusCode

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -10,11 +10,10 @@ const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
-    res.status(200);
     res.send('Welcome to the payment system');
 });
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('API available on localhost port 7865');
 });
 
